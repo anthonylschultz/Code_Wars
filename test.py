617b02b416208871a99f6b963b34bb9fb95c07e8
anthonylschultz <anthony.schultz@liveramp.com>
2020-04-03 13:22:51 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,38 @@
-def test(d):
-return d
-green
-blue
-orange
-purple
 
-green
-black
-white
+
+
+
+
+
+
+str = "Jędrzej Błądziński"
+def correct_polish_letters(str): 
+    '''
+    parameter:
+    str
+
+    returns:
+    str with replaced polish characters
+    '''
+
+    lst_ = []
+    polish = {'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś':'s', 'ź': 'z', 'ż': 'z'}
+    for i in str:
+        if i in polish.keys():
+            str = str.replace(i, polish[i])
+    return str
+
+
+
+solution("camelCasing")  ==  "camel Casing"
+str = "camelCasing"
+def solution(str):
+    for i in str:
+        new_str = str.split(i.isupper())
+    return new_str
+
+
+    for i in str:
+        if i.isupper():
+            new_str = str.split(i.isupper())
+        return new_str
