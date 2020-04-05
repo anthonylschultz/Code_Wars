842d16d389b387a96af12cd30cfb3319635c6744
anthonylschultz <anthony.schultz@liveramp.com>
2020-04-05 16:47:25 -0700

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -1,3 +1,15 @@
+# 20200405
+
+# Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+# Examples:
+# Input: 21445 Output: 54421
+
+# Input: 145263 Output: 654321
+
+# Input: 123456789 Output: 987654321
+
+
 # refactored
 def Descending_order(num):
     return int("".join(sorted(str(num), reverse = True)))
@@ -10,16 +22,8 @@ def descending_order(num):
     integer = int("".join(map(str, y)))
     return integer # convert back to int
 
-Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
-Examples:
-Input: 21445 Output: 54421
-
-Input: 145263 Output: 654321
-
-Input: 123456789 Output: 987654321
-
-
+# 20200402
 str = "Jędrzej Błądziński"
 def correct_polish_letters(str): 
     '''
